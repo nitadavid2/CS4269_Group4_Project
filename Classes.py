07a984ba4736ac8229ed703ad1401d4130dc13f6
Vashni <boamahvashni@gmail.com>
2021-02-21 19:37:00 -0600

    class updatde 2

--- Classes.py
+++ Classes.py
@@ -4,11 +4,27 @@ import ReadInFile
 
 class Country:
     def __init__(self, countryName):
-        self.resources = dict;
+        self.resources = dict
         self.name = countryName
 
     def state_quality(self):
-        return
+        return 0
 
-    def readCountryResoucrce(self):
+    def readCountryResource(self):
         self.resources
+
+    def calc_rawutility(self):
+        return 0
+
+
+class State:
+    def __init__(self, depth, countries, schedule):
+        self.countries = countries
+        self.depth = depth
+        self.path = schedule
+
+    def findSuccessor(self, actions):
+        return 0
+
+    def discounted_reward(self,country):
+        return 0
