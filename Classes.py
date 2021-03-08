2481e630a3923f58d85a5b3dc917711adfcc61fc
AndrewKarstrand <alkarstrand@gmail.com>
2021-03-07 20:41:24 -0600

    add timer for execution and add multiprocessing to succesor evaluation(not working)

--- Classes.py
+++ Classes.py
@@ -2,6 +2,15 @@ import ResourceQuality
 import BasicOperations
 import copy
 import math
+from multiprocessing import Pool
+from multiprocessing import freeze_support
+
+n_processors = 4
+
+
+def evalTransform(args):
+    state, type = args
+    state.eval(state, type)
 
 
 class Country:
@@ -25,6 +34,7 @@ class State:
         self.countries = countries
         self.depth = depth
         self.path = schedule
+        resources = self.countries['MyCountry'].resources
 
     # Define the notion of '<' for a State, used in PriorityQueue when priority of
     # two states is otherwise equal. We prefer the lower depth in that case
@@ -47,6 +57,88 @@ class State:
                         new_my_country.participation_prob = self.country_participation_probability\
                             (new_my_country.discounted_reward, 0, 1)
                         successor_list.append(new_state)
+
+        def successors_for_transfer(path, countries, depth):
+            for quantity in quantity_choices:
+                positive_resource_unit_prices = transfer_unit_prices[0:6]
+                for desired_resource in positive_resource_unit_prices:
+                    other_resources = transfer_unit_prices.copy()
+                    other_resources.remove(desired_resource)
+                    for coun in countries:
+                        if coun != 'MyCountry':
+                            for r in other_resources:
+                                a1, b1, c1 = BasicOperations.transfer(coun, country, countries[coun].resources,
+                                                                      resources, desired_resource[0], quantity)
+                                trade_amount = desired_resource[1] * quantity / abs(r[1])
+                                if 'Waste' in r[0]:
+                                    a2, b2, c2 = BasicOperations.transfer(coun, country, b1,
+                                                                          c1, r[0], trade_amount)
+                                else:
+                                    a2, c2, b2 = BasicOperations.transfer(country, coun, c1,
+                                                                          b1, r[0], trade_amount)
+                                if a1 and a2:
+                                    path_to_update = copy.deepcopy(path)
+                                    countries_to_update = copy.deepcopy(countries)
+                                    path_to_update.append(a1)
+                                    path_to_update.append(a2)
+                                    countries_to_update[coun].resources = b2
+                                    countries_to_update[country].resources = c2
+                                    new_state = State(depth + 1, countries_to_update, path_to_update)
+                                    new_my_country = new_state.countries[country]
+                                    new_other_country = new_state.countries[coun]
+                                    new_my_country.discounted_reward = self.discounted_reward(0.9, new_my_country)
+                                    new_other_country.discounted_reward = self.discounted_reward(0.9, new_other_country)
+                                    new_my_country.participation_prob = self.country_participation_probability \
+                                        (new_my_country.discounted_reward, 0, 1)
+                                    new_other_country.participation_prob = self.country_participation_probability \
+                                        (new_other_country.discounted_reward, 0, 1)
+                                    successor_list.append(new_state)
+
+        quantity_choices = [2 ** 0, 2 ** 1, 2 ** 2, 2 ** 3, 2 ** 4, 2 ** 5, 2 ** 6]
+        transform_types = ['housing', 'food', 'electronics', 'metalAlloys']
+        transfer_unit_prices = [('metalElements', 100), ('timber', 80), ('metalAlloys', 90),
+                                ('electronics', 200), ('food', 60), ('water', 20),
+                                ('metalAlloysWaste', -100), ('housingWaste', -150),
+                                ('electronicsWaste', -200), ('foodWaste', -100)]
+        successor_list = list()
+        country = 'MyCountry'
+        resources = self.countries[country].resources
+        successors_for_transform(self.path, self.countries, self.depth)
+        successors_for_transfer(self.path, self.countries, self.depth)
+
+        return successor_list
+
+
+
+    def eval(self, type, other):
+        quantity_choices = [2 ** 0, 2 ** 1, 2 ** 2, 2 ** 3, 2 ** 4, 2 ** 5, 2 ** 6]
+        transform_types = ['housing', 'food', 'electronics', 'metalAlloys']
+        transfer_unit_prices = [('metalElements', 100), ('timber', 80), ('metalAlloys', 90),
+                                ('electronics', 200), ('food', 60), ('water', 20),
+                                ('metalAlloysWaste', -100), ('housingWaste', -150),
+                                ('electronicsWaste', -200), ('foodWaste', -100)]
+        country = 'MyCountry'
+        resources = self.countries[country].resources
+        for quantity in quantity_choices:
+            a, b = BasicOperations.transform(country, resources, quantity, type)
+            if a:
+                path_to_update = copy.deepcopy(path)
+                countries_to_update = copy.deepcopy(countries)
+                path_to_update.append(a)
+                countries_to_update[country].resources = b
+                new_state = State(depth + 1, countries_to_update, path_to_update)
+                new_my_country = new_state.countries[country]
+                new_my_country.discounted_reward = self.discounted_reward(0.9, new_my_country)
+                new_my_country.participation_prob = self.country_participation_probability\
+                    (new_my_country.discounted_reward, 0, 1)
+                successor_list.append(new_state)
+
+    def findSuccessorMP(self):
+
+        def successors_for_transform(path, countries, depth):
+            with Pool(processes=n_processors) as pool:
+                args = [(self, transform) for transform in transform_types]
+                pool.map(evalTransform, args)
 
         def successors_for_transfer(path, countries, depth):
             for quantity in quantity_choices:
--- Scheduler.py
+++ Scheduler.py
@@ -5,6 +5,7 @@ from Classes import Country
 import ResourceQuality
 import math
 import queue
+import time
 
 
 def schedule_probability(probability_for_countries):
@@ -14,6 +15,73 @@ def schedule_probability(probability_for_countries):
 def expected_utility(probability, self_discounted_reward):
     return probability * self_discounted_reward
 
+
+def mp_a_star_search(start, depth):
+    # We will use a priority queue for the A* implementation.
+    # We will expand the frontier each time by expanding the "best path" using the
+    # (expected utility - utility) as heuristic.
+    search_queue = queue.PriorityQueue()
+
+    # We will have a queue of possible, valid paths
+    solution_queue = queue.PriorityQueue()
+
+    # A "schedule" is defined as list of actions (transfers or transforms)
+    # Query the .path attribute of a state for path to current state.
+    # Query .countries for states of countries currently in state.
+    # Query .depth to get current depth
+
+    # Initialize search_queue
+    for suc in start.findSuccessor():
+        # Get schedule probability
+        country_probs = [c.participation_prob for c in suc.countries.values()]
+        total_prob = schedule_probability(country_probs)
+
+        # Get discounted reward of MyCountry
+        dr = suc.countries['MyCountry'].discounted_reward
+
+        # Calculate E.U.
+        eu = expected_utility(total_prob, dr)
+
+        # Use -util since PriorityQueue.get() takes item with lowest priority
+        search_queue.put((-eu, suc))
+
+    # Explore search_queue
+    while search_queue.qsize() > 0:
+        next_item = search_queue.get()
+        next_state_value, next_state = next_item[0], next_item[1]
+
+        # Push current state to queue storing possible solutions
+        solution_queue.put(next_item)
+
+        # Before we go on, check current depth
+        if next_state.depth < depth:
+            # Now generate successors
+            successors = next_state.findSuccessor()
+            for suc in next_state.findSuccessor():
+                # Get schedule probability
+                country_probs = [c.participation_prob for c in suc.countries.values()]
+                total_prob = schedule_probability(country_probs)
+
+                # Get discounted reward of MyCountry
+                dr = suc.countries['MyCountry'].discounted_reward
+
+                # Calculate E.U.
+                eu = expected_utility(total_prob, dr)
+
+                # Add to queue
+                search_queue.put((-eu, suc))
+
+    # Return best option
+    answer_item = solution_queue.get()
+    answer_value = -answer_item[0]
+    answer_path = answer_item[1].path
+
+    # TODO: Remove after testing.
+    print("Number of solutions: ", solution_queue.qsize())
+    print("Best solution EU: ", answer_value)
+    print("Best Path: ", answer_path)
+
+    return answer_path
 
 def a_star_search(start, depth):
     # We will use a priority queue for the A* implementation.
@@ -81,7 +149,6 @@ def a_star_search(start, depth):
 
     return answer_path
 
-
 def depth_first_search(start, depth, utility):
     # TODO: Implement depth first search algorithm
     return 0
@@ -92,12 +159,16 @@ def breadth_first_search(start, depth, utility):
     return 0
 
 
+
+
 if __name__ == '__main__':
     country_dict = ReadInFile.getCountryDict()
     resource_dict = ReadInFile.getResourceDict()
     start_state = State(0, country_dict, [])
     my_country = country_dict['MyCountry']
     # TODO: Test and Run Search
-
+    start = time.perf_counter()
     test = a_star_search(start_state, 1)
+    end = time.perf_counter()
     print(test)
+    print(f"Search time: {end - start:0.4f}")
