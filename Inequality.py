73e11ce8b88a4cdfd1e3758a601f8ec1cdd3c2fe
AndrewKarstrand <alkarstrand@msn.com>
2021-04-05 16:24:44 -0500

    add relative quality function

--- Inequality.py
+++ Inequality.py
@@ -39,3 +39,34 @@ def mean_log_dev(state):
 
     # MLD is now calculated
     return MLD
+
+def actor_rel_quality(state):
+
+    total_state_qual = 0
+    n_countries = 0
+    constant_offset = 0
+
+    for country_name in state.countries:
+        country = state.countries[country_name]
+
+        # Call the State Quality Function that we already defined. This calculates state quality
+        # according to the Resource weights and thresholds that are pre-defined.
+        quality = ResourceQuality.getStateQuality(country.resources)
+
+        # Determine xbar, N, and normalizing constant (to ensure all scores are >= 1 for ln() analysis later.
+        total_state_qual += quality
+        n_countries += 1
+
+        # Adjust constant offset, if applicable
+        if quality < 1:
+            if (1 - quality) > constant_offset:
+                constant_offset = 1 - quality
+
+    # Now determine the x-bar and N
+    xbar = (total_state_qual / n_countries) + constant_offset
+
+    # Now iterate through countries again for final calculation steps
+    ARQ = 0
+    xi = ResourceQuality.getStateQuality(state.countries["MyCountry"].resources) + constant_offset
+    ARQ = xbar / xi
+    return ARQ
